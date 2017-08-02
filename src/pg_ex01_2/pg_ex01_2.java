879ee87f7c78e51754e7b15cfb38deb58205e48c
user <user@192.168.128.100>
2017-08-02 22:12:32 +0900

    最初のコミット

---
+++ src/pg_ex01_2/pg_ex01_2.java
@@ -0,0 +1,46 @@
+package pg_ex01_2;
+
+public class pg_ex01_2 {
+	public static void main(String[] args) {
+		change(1);
+	}
+	public static void change(int x) {
+		if ((x >= 1) && (x <= 20)) {
+			if (x >= 10){
+				System.out.print("X");
+				x = x - 10;
+			}
+			else{
+			}
+
+			if ((x + 1) % 5 == 0){
+				System.out.print("I");
+				x = x + 1;
+			}
+			else{
+			}
+
+			if (x == 10){
+				System.out.print("X");
+				x = x - 10;
+			}
+			else{
+			}
+
+			if (x >= 5){
+				System.out.print("V");
+				x = x - 5;
+			}
+			else{
+			}
+
+			for (int i=x ;i > 0;i--) {
+				System.out.print("I");
+			}
+
+		}
+		else {
+			System.out.println("変換できません");
+		}
+	}
+}
